d501593ee74b2ca32ebbc52e84bb2587f1b4d028
malviaje <lukas.arancibia93@gmail.com>
2019-05-16 14:06:14 -0400

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -8,7 +8,7 @@ process.env.PORT = process.env.PORT || 3000;
 //ENTORNO
 //=========================
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 //=========================
 //Base de datos
